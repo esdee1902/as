0c24b5997ad490dc52af4a80544011b2f4489518
Hoai Cao <hoaicao14374@gmail.com>
2016-11-02 15:35:07 +0700

    dep trai is real

---
+++ Git/src/gitb.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+
+/**
+ * @author Hoai Cao
+ * T154888
+ */
+public class gitb extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		gitb f = new gitb();
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+
+}
